09274308f831c85e1b176fe27e2aa29a873df177
mholliday6611 <mholliday6611@gmail.com>
2017-11-16 20:27:38 -0500

    props

---
+++ src/Todo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+class Todo extends Component {
+	render(){
+	return(
+		<div>
+		<button onClick={this.props.but}>Press</button>
+		</div>
+	)
+	}
+  
+
+}
+
+export default Todo
